55cc86d94f518b7765aba250520650ce19aa0cb6
Ruben Martin <Ruben Martin>
2016-12-15 21:25:53 +0100

    Added all the changes for the Tests

--- src/todobackend/settings/test.py
+++ src/todobackend/settings/test.py
@@ -4,6 +4,22 @@ import os
 # Databese
 # https://docs.djangorproject.com/en/1.8/ref/seetings/#databases
 
+INSTALLED_APPS += ('django.nose')
+TEST_RUNNER = 'django.nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
+NOSE_ARGS = [
+	'--verbosity=2',
+	'--nologcapture',
+	'--with-coverage',
+	'--cover-package=todo',
+	'--with-spec',
+	'--spec-color',
+	'--with-xunit',
+	'--xunit-file=%s/unittests.xml' % TEST_OUTPUT_DIR,
+	'--cover-xml',
+	'--cover-xml-file=%s/coverage.xml' % TEST_OUTPUT_DIR,
+]
+
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
